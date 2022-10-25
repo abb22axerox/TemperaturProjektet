39ff75c324c6980e8433469aa0cb64f11ab56e46
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2022-10-25 10:13:07 +0200

    er456

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1Z3eWF5XuyBvX8XPetVs76hy5osSA6KM",
-    authDomain: "skogens-thm.firebaseapp.com",
-    databaseURL: "https://skogens-thm-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "skogens-thm",
-    storageBucket: "skogens-thm.appspot.com",
-    messagingSenderId: "666647829702",
-    appId: "1:666647829702:web:ecefb34b3c3dda0b5b0bc7"
-  };
+  apiKey: "AIzaSyDzJMZg8uoitcrbyop9PPu5aQpuw2ja38Q",
+  authDomain: "test-7552f.firebaseapp.com",
+  databaseURL: "https://test-7552f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-7552f",
+  storageBucket: "test-7552f.appspot.com",
+  messagingSenderId: "107856682690",
+  appId: "1:107856682690:web:599547b5814614132be55b"
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -16,34 +17,39 @@ const db = firebase.database();
 
 var room2Ref = db.ref("rum2");
 
+let timeList = []
+
+let tempList = []
+
 room2Ref.once("value").then((snapshot) => {
-    let r2r = snapshot.val()
-    // console.log(r2r)
-    let tempTime = r2r.split(',')
-    // console.log(tempTime)
-    let IDKWhatToNameThis = []
-    for(i = 0; i < tempTime.length; i++){
-        IDKWhatToNameThis.push(tempTime[i].split(' '))
-        // console.log(IDKWhatToNameThis)
-        }
-    // console.log(IDKWhatToNameThis[0][1] + ' turtles eat toast')
-    
-    let timeList = []
+  let r2r = snapshot.val()
+  // console.log(r2r)
+  let tempTime = r2r.split(',')
+  // console.log(tempTime)
+  let IDKWhatToNameThis = []
+  for(i = 0; i < tempTime.length; i++){
+      IDKWhatToNameThis.push(tempTime[i].split(' '))
+      // console.log(IDKWhatToNameThis)
+      }
+  // console.log(IDKWhatToNameThis[0][1] + ' turtles eat toast')
+  
 
-    for(i=0; i < IDKWhatToNameThis.length; i++){
-        timeList.push(IDKWhatToNameThis[i][0])
-        // console.log(timeList)
-    }
 
-    let tempList = []
+  for(i=0; i < IDKWhatToNameThis.length; i++){
+      timeList.push(IDKWhatToNameThis[i][0])
+      // console.log(timeList)
+  }
 
-    for(i=0; i < IDKWhatToNameThis.length; i++){
-        tempList.push(IDKWhatToNameThis[i][1])
-        // console.log(tempList)
-    }
-    console.log(timeList)
-    console.log(tempList)
+
+
+  for(i=0; i < IDKWhatToNameThis.length; i++){
+      tempList.push(IDKWhatToNameThis[i][1])
+      // console.log(tempList)
+  }
+  console.log(timeList)
+  console.log(tempList)
 })
+
 
 //Load the graph
 graphFunction()
@@ -52,7 +58,7 @@ graphFunction()
 var a = 0
 var b = 0
 
-var timeArray = [0,10,25]
+var timeArray = timeList
 
 console.log(timeList)
 
@@ -139,7 +145,7 @@ console.log(timeList)
   sensor1.addEventListener('change', function () {
     if (sensor1.checked) {
       if(chosen == 1){
-        temp1Array = [8,19,44]
+        temp1Array = tempList
         graphFunction()
       }
       if(chosen == 2){
