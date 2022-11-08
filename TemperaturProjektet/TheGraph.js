e41ed74dbfd9cc9062a73a7fde78e5a34b3563a7
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2022-11-08 08:09:02 +0100

    sdfg

--- TemperaturProjektet/TheGraph.js
+++ TemperaturProjektet/TheGraph.js
@@ -1,51 +1,3 @@
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDzJMZg8uoitcrbyop9PPu5aQpuw2ja38Q",
-  authDomain: "test-7552f.firebaseapp.com",
-  databaseURL: "https://test-7552f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "test-7552f",
-  storageBucket: "test-7552f.appspot.com",
-  messagingSenderId: "107856682690",
-  appId: "1:107856682690:web:599547b5814614132be55b"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database();
-
-var room2Ref = db.ref("rum2");
-
-let timeList = []
-
-let tempList = []
-
-room2Ref.once("value").then((snapshot) => {
-  let r2r = snapshot.val()
-  // console.log(r2r)
-  let tempTime = r2r.split(',')
-  // console.log(tempTime)
-  let IDKWhatToNameThis = []
-  for(i = 0; i < tempTime.length; i++){
-      IDKWhatToNameThis.push(tempTime[i].split(' '))
-      // console.log(IDKWhatToNameThis)
-      }
-  
-  for(i=0; i < IDKWhatToNameThis.length; i++){
-      timeList.push(IDKWhatToNameThis[i][0])
-      // console.log(timeList)
-  }
-
-  for(i=0; i < IDKWhatToNameThis.length; i++){
-      tempList.push(IDKWhatToNameThis[i][1])
-      // console.log(tempList)
-  }
-  console.log(timeList)
-  console.log(tempList)
-})
-
-
 //Load the graph
 graphFunction()
 
@@ -53,9 +5,9 @@ graphFunction()
 var a = 0
 var b = 0
 
-var timeArray = timeList
+var timeArray = [0,10,25]
 
-console.log(timeList)
+// console.log(timeList)
 
 //Temperature
   // Sensor 1
@@ -140,7 +92,7 @@ console.log(timeList)
   sensor1.addEventListener('change', function () {
     if (sensor1.checked) {
       if(chosen == 1){
-        temp1Array = tempList
+        temp1Array = [8,19,44]
         graphFunction()
       }
       if(chosen == 2){
